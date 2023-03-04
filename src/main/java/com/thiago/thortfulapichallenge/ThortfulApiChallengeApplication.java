7a532d77a502544f02d0870e64de7212e6782c54
Thiago Felipe <thiagofg.eng@gmail.com>
2023-03-03 23:03:15 -0300

    Initial commit

---
+++ src/main/java/com/thiago/thortfulapichallenge/ThortfulApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.thortfulapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThortfulApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThortfulApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/thortfulapichallenge/ThortfulApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.thortfulapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThortfulApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
