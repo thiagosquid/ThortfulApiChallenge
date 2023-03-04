1afc5f676a5bd465f0c45cadf35387ac8a49f863
Thiago Felipe <thiagofg.eng@gmail.com>
2023-03-04 11:50:27 -0300

    Fix attribute type and remove lombok annotation

--- src/main/java/com/thiago/thortfulapichallenge/model/Location.java
+++ src/main/java/com/thiago/thortfulapichallenge/model/Location.java
@@ -10,7 +10,7 @@ public class Location {
     private String city;
     private String state;
     private String country;
-    private Long postcode;
+    private String postcode;
     private Coordinates coordinates;
     private Timezone timezone;
 
